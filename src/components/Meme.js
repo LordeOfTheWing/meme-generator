4db07e25f3f5e618a720e1cc4d0c7ffe815551bc
Clinton Mainya <klyntonmainya@gmail.com>
2022-02-28 18:57:02 +0300

    Added the functionality to fetch the meme images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import memesData from '../memesData';
 
 const Meme = () => {
+  const [memeImage, setMemeImage] = useState('');
+
+  const handleSubmit = () => {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length) + 1;
+    const url = memesArray[randomNumber].url;
+    setMemeImage(url);
+  };
+
   return (
     <main>
       <div className='form'>
         <input className='form--input' type='text' placeholder='Top Text' />
         <input className='form--input' type='text' placeholder='Bottom Text' />
-        <button className='form--button'>Get a new meme image</button>
+        <button onClick={handleSubmit} className='form--button'>
+          Get a new meme image
+        </button>
       </div>
+      <img className='meme--image'src={memeImage} alt={memeImage} />
     </main>
   );
 };
