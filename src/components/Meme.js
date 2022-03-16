da1a47237680ccef3b3cf118f6a43dcf66fe791b
Clinton Mainya <klyntonmainya@gmail.com>
2022-03-16 21:13:50 +0300

    Add custom meme edit feature

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,8 +3,8 @@ import memesData from '../memesData';
 
 const Meme = () => {
   const meme = {
-    topText: '',
-    bottomText: '',
+    topText: 'One does not simply',
+    bottomText: 'Walk into Mordor',
     randomImage: 'http://i.imgflip.com/1bij.jpg',
   };
 
@@ -25,20 +25,48 @@ const Meme = () => {
     });
   };
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setMemeImage((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  };
+
   return (
     <main>
       <div className='form'>
-        <input className='form--input' type='text' placeholder='Top Text' />
-        <input className='form--input' type='text' placeholder='Bottom Text' />
+        <input
+          name='topText'
+          className='form--input'
+          type='text'
+          placeholder='Top Text'
+          value={memeImage.topText}
+          onChange={handleChange}
+        />
+        <input
+          name='bottomText'
+          className='form--input'
+          type='text'
+          placeholder='Bottom Text'
+          value={memeImage.bottomText}
+          onChange={handleChange}
+        />
         <button onClick={getMemeImage} className='form--button'>
           Get a new meme image
         </button>
       </div>
-      <img
-        className='meme--image'
-        src={memeImage.randomImage}
-        alt={memeImage.randomImage}
-      />
+      <div className='meme'>
+        <img
+          className='meme--image'
+          src={memeImage.randomImage}
+          alt={memeImage.randomImage}
+        />
+        <h2 className='meme--text top'>{memeImage.topText}</h2>
+        <h2 className='meme--text bottom'>{memeImage.bottomText}</h2>
+      </div>
     </main>
   );
 };
